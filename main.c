f88fcd7591603b6c70cabc7037652e7e8ee4b9cd
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-21 22:23:50 -0400

    update

--- main.c
+++ main.c
@@ -3,3 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+// Function that generate a number in
+// the range [1, N] and checks if the
+// generated number is the same as the
+// guessed number or not
